ddeac42d351cf845ca1ad985dd683ffa27e51d43
Controller Works <info@controller.works>
2023-05-26 08:34:34 -0400

    got keymap working

---
+++ keyboards/crkbd/keymaps/cornedc/rules.mk
@@ -0,0 +1,8 @@
+BOOTLOADER = atmel-dfu
+MOUSEKEY_ENABLE = no     # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+VIA_ENABLE      = no    # Enable VIA
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+RGB_MATRIX_ENABLE = yes
